b6c167ea67d6316f8ea04ea853182d506551c850
Dong1414 <homar1@naver.com>
2021-01-10 16:05:20 +0900

    member, DAO, 컨트롤러 도입

--- src/com/sbs/example/jspCommunity/container/Container.java
+++ src/com/sbs/example/jspCommunity/container/Container.java
@@ -1,14 +1,33 @@
 package com.sbs.example.jspCommunity.container;
 
+import com.sbs.example.jspCommunity.controller.ArticleController;
+import com.sbs.example.jspCommunity.controller.usr.MemberController;
 import com.sbs.example.jspCommunity.dao.ArticleDao;
+
 import com.sbs.example.jspCommunity.service.ArticleService;
+import com.sbs.example.jspCommunity.dao.MemberDao;
+import com.sbs.example.jspCommunity.service.MemberService;
 
 public class Container {
-	public static ArticleService articleService;
+	
 	public static ArticleDao articleDao;
+	public static MemberDao memberDao;
+	
+	public static ArticleController articleController;
+	public static MemberController memberController;
+	
+	public static ArticleService articleService;
+	public static MemberService memberService;
+	
 
 	static {
 		articleDao = new ArticleDao();
+		memberDao = new MemberDao();
+		
 		articleService = new ArticleService();
+		memberService = new MemberService();
+		
+		memberController = new MemberController();
+		articleController = new ArticleController();
 	}
 }
